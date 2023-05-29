b4099b7e1a165ea443def51b7fee9a9511dc8081
Kim Soo Hyun <spancer1@naver.com>
2023-05-29 16:26:17 +0900

    Nav 수정 및 Type 추가

--- src/App.js
+++ src/App.js
@@ -14,10 +14,14 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Default from './components/survey/Default';
 import Type_A from './components/survey/Type_A';
+import Type_B from './components/survey/Type_B';
+import Type_C from './components/survey/Type_C';
+import Type_D from './components/survey/Type_D';
+import Type_E from './components/survey/Type_E';
 import Footer from './components/Footer';
 function App() {
-  const [activeOption, setActiveOption] = useState('default');
-
+  const [defaultOption, setDefaultOption] = useState('default');
+  const [activeOption, setActiveOption] = useState('type_A');
   return (
     <React.Fragment>
       <CssBaseline />
@@ -25,12 +29,23 @@ function App() {
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
           <Header />
           <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Nav activeOption={activeOption} setActiveOption={setActiveOption} />
+            <Nav defaultOption = {defaultOption} setDefaultOption = {setDefaultOption} activeOption={activeOption} setActiveOption={setActiveOption} />
             <Box sx={{ flex: 1 }}>{(() => {
-              if (activeOption === 'default') 
+              if (defaultOption === 'default') 
                 return <Default />;
               else {
-                return <Type_A />;
+                if(activeOption==='type_a')
+                  return <Type_A />;
+                else if(activeOption==='type_b')
+                  return <Type_B />;
+                else if(activeOption==='type_c')
+                  return <Type_C />;
+                else if(activeOption==='type_d')
+                  return <Type_D />;
+                else if(activeOption==='type_e')
+                  return <Type_E />;
+                else
+                return <Default />;
               }})()}
             </Box>
           </Box>
@@ -40,5 +55,6 @@ function App() {
     </React.Fragment>
   );
 }
+
 
 export default App
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,81 @@
 import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 
-function Nav({ activeOption, setActiveOption }) {
-  const isActive = (option) => option === activeOption;
-
-  return (
-    <nav>
-      <div
-        style={{
-          border: '1px solid gray',
-          backgroundColor: isActive('default') ? 'gray' : 'white',
-        }}
-        onClick={() => setActiveOption('default')}
-      >
-        <li>공통 질문 관리</li>
-      </div>
-      <div
-        style={{
-          border: '1px solid gray',
-          backgroundColor: isActive('type_a') ? 'gray' : 'white',
-        }}
-        onClick={() => setActiveOption('type_a')}
-      >
-        <li>사용자 유형별 질문 관리</li>
-      </div>
-    </nav>
-  );
-}
-
+function Nav({ defaultOption, setDefaultOption, activeOption, setActiveOption}) {
+    const isActive = (option) => option === activeOption;
+    const is_Default =(option) => option === defaultOption; 
+    return (
+      <nav>
+        <div
+          style={{
+            border: '1px solid gray',
+            backgroundColor: is_Default('default') ? 'gray' : 'white',
+          }}
+          onClick={() => setDefaultOption('default')}
+        >
+          <li>공통 질문 관리</li>
+        </div>
+        <div
+          style={{
+            border: '1px solid gray',
+            backgroundColor: is_Default('userTypeManagement') ? 'gray' : 'white',
+          }}
+          onClick={() => setDefaultOption('userTypeManagement')}
+        >
+          <li>사용자 유형별 질문 관리</li>
+        </div>
+        {is_Default('userTypeManagement') && (
+          <>
+            <div
+              style={{
+                border: '1px solid gray',
+                backgroundColor: isActive('type_a') ? 'gray' : 'white',
+              }}
+              onClick={() => setActiveOption('type_a')}
+            >
+              <li>A 유형</li>
+            </div>
+            <div
+              style={{
+                border: '1px solid gray',
+                backgroundColor: isActive('type_b') ? 'gray' : 'white',
+              }}
+              onClick={() => setActiveOption('type_b')}
+            >
+              <li>B 유형</li>
+            </div>
+            <div
+              style={{
+                border: '1px solid gray',
+                backgroundColor: isActive('type_c') ? 'gray' : 'white',
+              }}
+              onClick={() => setActiveOption('type_c')}
+            >
+              <li>C 유형</li>
+            </div>
+            <div
+              style={{
+                border: '1px solid gray',
+                backgroundColor: isActive('type_d') ? 'gray' : 'white',
+              }}
+              onClick={() => setActiveOption('type_d')}
+            >
+              <li>D 유형</li>
+            </div>
+            <div
+              style={{
+                border: '1px solid gray',
+                backgroundColor: isActive('type_e') ? 'gray' : 'white',
+              }}
+              onClick={() => setActiveOption('type_e')}
+            >
+              <li>E 유형</li>
+            </div>
+          </>
+        )}
+        
+      </nav>
+    );
+  }
+  
 export default Nav;
